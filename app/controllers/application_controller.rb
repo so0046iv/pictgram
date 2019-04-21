5f59869ad2c976dff1aac823f688ce343a020c03
so0046iv <so0046iv@yahoo.co.jp>
2019-04-21 21:26:06 +0900

    rails06_homework

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  add_flash_types :success, :info, :warning, :danger
+
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  helper_method :logged_in?
+  def logged_in?
+    !current_user.nil?
+  end
+end
